3c920491358938b0504abf9b63c50b35e25af6f1
venti1112 <3788365896@qq.com>
2026-07-05 18:33:21 +0800

    修复架构名问题

--- tar_jdk.sh
+++ tar_jdk.sh
@@ -30,4 +30,4 @@ cd jreout
 # Strip in place all .so files thanks to the ndk
 find ./ -name '*.so' -execdir $NDK/toolchains/llvm/prebuilt/linux-x86_64/${NDK_PREBUILT_ARCH}-linux-android/bin/strip {} \;
 
-tar cJf ../jre17-${TARGET_SHORT}-`date +%Y%m%d`-${JDK_DEBUG_LEVEL}.tar.xz .
+tar cJf ../jre17-${TARGET_JDK}-`date +%Y%m%d`-${JDK_DEBUG_LEVEL}.tar.xz .
